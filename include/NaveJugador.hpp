398cb8be3f00d7da731e20a3a9332299833836f2
Alvaro Ortiz <A23110160@live.ceti.mx>
2024-06-15 12:29:50 -0600

    movimiento decente A

--- include/NaveJugador.hpp
+++ include/NaveJugador.hpp
@@ -2,51 +2,53 @@
 #include <SFML/Graphics.hpp>
 #include <Subscriptor.hpp>
 #include <iostream>
+#include <cmath>
 
 class NaveJugador : public Subscriptor
 {
 private:
-    sf::CircleShape circle;
+    sf::ConvexShape triangle;
 
 public:
     NaveJugador(/* args */)
     {
-        circle.setRadius(25);
-        circle.setFillColor(sf::Color::Red);
-        circle.setPosition(375, 275);
+        triangle.setPointCount(3);
+        triangle.setPoint(0, sf::Vector2f(-25, -25));
+        triangle.setPoint(1, sf::Vector2f(25, -25));
+        triangle.setPoint(2, sf::Vector2f(0, 25));
+        triangle.setFillColor(sf::Color::Red);
+        triangle.setPosition(375, 275);
     }
     ~NaveJugador() {}
     void Actualizar(sf::Event evento)
     {
-        // Controlar el objeto circular con las flechas del teclado
-        float velocidadMaxima = 100.0f;
-        float aceleracion = 5.0f;
-
+        // Controlar el objeto triangular con las flechas del teclado
         if (evento.key.code == sf::Keyboard::Left){
-            circle.move(-5, 0);
+           // triangle.move(-5, 0);
+            triangle.rotate(-2);
         }
-        else if (evento.key.code == sf::Keyboard::Right){
-            circle.move(5, 0);
+        else if (evento.key.code == sf::Keyboard::Right)
+{
+
+            triangle.rotate(2);
         }
-        else if (evento.key.code == sf::Keyboard::Up){
-            sf::Vector2f currentVelocity = circle.getPosition();
-            sf::Vector2f newVelocity = currentVelocity + sf::Vector2f(0, -aceleracion);
-            if (newVelocity.y < -velocidadMaxima) {
-                newVelocity.y = -velocidadMaxima;
+            else if (evento.key.code == sf::Keyboard::Up) {
+                float angle = triangle.getRotation();
+                float radians = angle * 3.14159 / 180.0;
+                float dx = std::sin(radians) * 5;
+                float dy = -std::cos(radians) * 5;
+                triangle.move(dx, dy);
             }
-            circle.setPosition(newVelocity);
-        }
-        else if (evento.key.code == sf::Keyboard::Down){
-            sf::Vector2f currentVelocity = circle.getPosition();
-            sf::Vector2f newVelocity = currentVelocity + sf::Vector2f(0, aceleracion);
-            if (newVelocity.y > velocidadMaxima) {
-                newVelocity.y = velocidadMaxima;
+            else if (evento.key.code == sf::Keyboard::Down) {
+                float angle = triangle.getRotation();
+                float radians = angle * 3.14159 / 180.0;
+                float dx = -std::sin(radians) * 5;
+                float dy = std::cos(radians) * 5;
+                triangle.move(dx, dy);
             }
-            circle.setPosition(newVelocity);
-        }
     }
     void Dibujar(sf::RenderWindow &window)
     {
-        window.draw(circle);
+        window.draw(triangle);
     }
 };
