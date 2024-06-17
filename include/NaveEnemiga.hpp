01c09d939096b81bc16113a0308b5e2fd57260bb
AAD23110162 <159966137+AAD23110162@users.noreply.github.com>
2024-06-16 22:43:51 -0600

    movimiento nave enemiga

--- include/NaveEnemiga.hpp
+++ include/NaveEnemiga.hpp
@@ -1,17 +1,10 @@
-#include <SFML/Graphics.hpp>
-#include <Subscriptor.hpp>
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
-#include <vector>
-#include <memory>
-
 class NaveEnemiga : public Subscriptor
 {
 private:
     sf::RectangleShape rectangle;
     float velocidad;
+    float direccionX;
+    float direccionY;
 
 public:
     NaveEnemiga()
@@ -20,28 +13,28 @@ public:
         rectangle.setFillColor(sf::Color::White);
         rectangle.setPosition(375, 275);
         velocidad = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 4.0f + 1.0f; // Velocidad aleatoria entre 1 y 5
+        direccionX = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 4.0f + 1.0f; // Dirección aleatoria inicial entre 1 y 5
+        direccionY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 4.0f + 1.0f; // Dirección aleatoria inicial entre 1 y 5
     }
     virtual ~NaveEnemiga() {}
 
     void Mover()
     {
         sf::Vector2f currentPosition = rectangle.getPosition();
-        float randomX = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 4.0f + 1.0f; // Random value between 1 and 5
-        float randomY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 4.0f + 1.0f; // Random value between 1 and 5
 
-        currentPosition.x += randomX;
-        currentPosition.y += randomY;
+        currentPosition.x += direccionX;
+        currentPosition.y += direccionY;
 
         rectangle.setPosition(currentPosition);
 
         if (currentPosition.x >= 750 || currentPosition.x <= 0)
         {
-            randomX = -randomX;
+            direccionX = -direccionX;
         }
 
         if (currentPosition.y >= 550 || currentPosition.y <= 0)
         {
-            randomY = -randomY;
+            direccionY = -direccionY;
         }
     }
 
@@ -50,7 +43,6 @@ public:
         window.draw(rectangle);
     }
 
-    // Implementación del método virtual puro de Subscriptor
     virtual void Actualizar(sf::Event evento) override
     {
         // Implementación específica para NaveEnemiga
